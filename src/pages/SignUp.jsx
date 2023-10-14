dcd7636d5c79f03fb60c5062b3518a99c3f765e7
wldnd2 <jiwoong000628@gmail.com>
2023-10-14 21:51:58 +0900

    Feat: 회원가입 폼 구현

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+import { TextField, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
@@ -6,9 +6,59 @@ export default function SignUp() {
   return (
     <Container>
       <h1>Sign Up</h1>
-      <Link to="/">
-        <Button variant="outlined">Back</Button>
-      </Link>
+      <FormContainer>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          // fullWidth
+          id="id"
+          label="User ID"
+          name="id"
+          autoComplete="id"
+          autoFocus
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          // fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          // fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          // fullWidth
+          name="password"
+          label="Confirm Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+      </FormContainer>
+      <ButtonContainer>
+        <Link to="/">
+          <Button variant="outlined">Sign in</Button>
+        </Link>
+        <Link to="/">
+          <Button variant="outlined">Back</Button>
+        </Link>
+      </ButtonContainer>
     </Container>
   );
 }
@@ -19,5 +69,21 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  width: 100%;
+`;
+
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
   width: 100%;
 `;
