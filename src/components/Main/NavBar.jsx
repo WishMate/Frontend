b50b853871b3d7b1e7502d34205eeaf1b1697c30
전지웅 <jeonjiwoong@jeonjiung-ui-MacBookPro-2.local>
2024-01-06 14:22:46 +0900

    Feat: nav bar 기본 구성 완료

--- src/components/Main/NavBar.jsx
+++ src/components/Main/NavBar.jsx
@@ -1,11 +1,77 @@
+import styled from "styled-components";
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
 
 export default function NavBar() {
   return (
-    <div>
-      <h1>NavBar</h1>
-      <Link to="/signin">Sign In</Link>
-      <Link to="/signup">Sign Up</Link>
-    </div>
+    <NavContainer>
+      <LogoContainer>
+        {/* 로고 이미지와 텍스트 */}
+        <ImageContainer>
+          <img src={logo} alt="Logo" />
+        </ImageContainer>
+        <LogoTextContainer>
+          <h2>Wish Mate</h2>
+        </LogoTextContainer>
+      </LogoContainer>
+      {/* 로그인 및 회원가입 */}
+      <SignContainer>
+        <Link to="/signin">
+          <h2>로그인</h2>
+        </Link>
+        <Link to="/signup">
+          <h2>회원가입</h2>
+        </Link>
+      </SignContainer>
+    </NavContainer>
   );
 }
+
+const NavContainer = styled.div`
+  ${({ theme }) => theme.flexRowSet()}
+  font-size: ${({ theme }) => theme.calcRem(20)};
+  color: ${({ theme }) => theme.colors.red};
+  background-color: ${({ theme }) => theme.colors.white};
+  padding: ${({ theme }) => theme.calcRem(20)};
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ImageContainer = styled.div`
+  margin-right: ${({ theme }) => theme.calcRem(10)};
+  img {
+    width: ${({ theme }) => theme.calcRem(40)};
+    height: auto;
+  }
+`;
+
+const LogoTextContainer = styled.div`
+  h2 {
+    margin: 0;
+  }
+`;
+
+const SignContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.black};
+    margin: 0 ${({ theme }) => theme.calcRem(10)};
+    h2 {
+      margin: 0;
+      font-weight: bold;
+      cursor: pointer;
+      transition: color 0.3s ease;
+      &:hover {
+        color: ${({ theme }) => theme.colors.blue};
+      }
+    }
+  }
+`;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,7 +3,6 @@ import NavBar from "../components/Main/NavBar";
 export default function MainPage() {
   return (
     <div>
-      <h1>Main Page</h1>
       <NavBar />
     </div>
   );
